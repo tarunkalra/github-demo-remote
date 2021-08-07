b012b554f4bde424788511f2786163a65a58710e
Tarun Kalra <ktarun213@gmail.com>
2021-08-07 18:50:10 +0530

    removed function headers

--- math.py
+++ math.py
@@ -1,18 +1,13 @@
-# Add implementation
 def add(x,y):
     return x+y
-# Subtract implementation
 def subtract(x,y):
     return x-y          #On master branch
-# Multiply implementation    
 def multiply(x,y):
     return x*y              #On bug branch
-#Divide implementation
 def divide(x,y):
     if y==0:                        #On master branch
         return DIVIDE_BY_0_ERROR
     else:
         return x/y
-# Square implementation
 def square(x):
     return x*x
